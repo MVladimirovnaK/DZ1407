65faa202b1b3f86c9024879680a314795162d479
mariya <mariyakudryashova19980308@gmail.com>
2023-07-19 21:32:56 +0300

    add02

---
+++ 02/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int number = new Random().Next();
+Console.WriteLine($"Случайное число: {number}");
+int sum = 0;
+
+while(number > 0)
+{
+    sum += number%10;
+    number/=10;
+}
+
+Console.WriteLine(sum);
